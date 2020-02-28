a186d4b35a035320cf801e95bfef176da6cc024b
alecmocatta <alec@mocatta.net>
2020-02-28 19:32:27 +0000

    add commented out fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use futures::StreamExt;
-use quinn::{Certificate, ClientConfig, ClientConfigBuilder, ServerConfigBuilder};
+use quinn::{
+	Certificate, ClientConfig, ClientConfigBuilder, ConnectionError, ServerConfigBuilder
+};
+use quinn_proto::TransportErrorCode;
 use rand::{rngs::SmallRng, Rng, SeedableRng};
 use std::{
 	net::{IpAddr, Ipv6Addr, SocketAddr, UdpSocket}, sync::Arc, time::Duration
@@ -15,28 +18,38 @@ async fn main() {
 	let cert = Certificate::from_der(&cert.serialize_der().unwrap()).unwrap();
 	let cert_chain = quinn::CertificateChain::from_certs(vec![cert.clone()]);
 
-	let transport = quinn::TransportConfig::default();
+	let mut transport = quinn::TransportConfig::default();
+	transport.idle_timeout(None).unwrap();
+	let transport = Arc::new(transport);
+
 	let mut server_config = quinn::ServerConfig::default();
-	server_config.transport = Arc::new(transport);
+	server_config.transport = transport.clone();
 	let mut server_config = ServerConfigBuilder::new(server_config);
 	server_config.certificate(cert_chain, key).unwrap();
+	let server_config = server_config.build();
 
 	let sock = UdpSocket::bind(SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 0)).unwrap();
 	let addr = sock.local_addr().unwrap();
 
-	let mut listen_endpoint = quinn::Endpoint::builder();
-	listen_endpoint.listen(server_config.build());
-	let (listen_endpoint, mut incoming) = listen_endpoint.with_socket(sock).unwrap();
+	let mut server_endpoint = quinn::Endpoint::builder();
+	server_endpoint.listen(server_config);
+	let (server_endpoint, mut incoming) = server_endpoint.with_socket(sock).unwrap();
 
-	let (connect_endpoint, _) = quinn::Endpoint::builder()
+	let (client_endpoint, _) = quinn::Endpoint::builder()
 		.bind(&SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 0))
 		.unwrap();
 
-	let iterations = 100_000;
+	let mut client_config = ClientConfig::default();
+	client_config.transport = transport.clone();
+	let mut client_config = ClientConfigBuilder::new(client_config);
+	client_config.add_certificate_authority(cert.clone()).unwrap();
+	let client_config = client_config.build();
+
+	let iterations = 100_000_000;
 	let mut shared_rng = rng.clone();
 	let shared_rng1 = shared_rng.clone();
 	let rng1 = SmallRng::from_rng(&mut rng).unwrap();
-	let connect_endpoint = &connect_endpoint;
+	let client_endpoint = &client_endpoint;
 	let sending = async move {
 		let mut shared_rng = shared_rng1;
 		let mut rng = rng1;
@@ -44,13 +57,21 @@ async fn main() {
 		for i in 0..iterations {
 			if sender.is_none() {
 				println!("{} sender open", i);
-				let mut client_config = ClientConfigBuilder::new(ClientConfig::default());
-				client_config.add_certificate_authority(cert.clone()).unwrap();
-				let connection = connect_endpoint
-					.connect_with(client_config.build(), &addr, "localhost")
-					.unwrap()
-					.await
-					.unwrap();
+				let connection = loop {
+					match client_endpoint
+						.connect_with(client_config.clone(), &addr, "localhost")
+						.unwrap()
+						.await
+					{
+						Ok(connection) => break connection,
+						Err(ConnectionError::ConnectionClosed(close))
+							if close.error_code == TransportErrorCode::SERVER_BUSY =>
+						{
+							delay_for(Duration::from_millis(10)).await
+						}
+						Err(err) => panic!("{:?}", err),
+					}
+				};
 				let mut sender_ = connection.connection.open_uni().await.unwrap();
 				sender_.write_all(&[1, 2]).await.unwrap();
 				sender = Some(sender_);
@@ -71,7 +92,7 @@ async fn main() {
 				println!("{} sender /send", i);
 			}
 			if rng.gen() {
-				delay_for(rng.gen_range(Duration::new(0, 0), Duration::from_millis(1))).await;
+				delay_for(rng.gen_range(Duration::new(0, 0), Duration::from_micros(500))).await;
 			}
 		}
 		if let Some(mut sender) = sender {
@@ -94,6 +115,8 @@ async fn main() {
 				println!("{} receiver /open", i);
 			} else if shared_rng.gen() {
 				println!("{} receiver close", i);
+				// let fin = receiver.as_mut().unwrap().read(&mut [0]).await.unwrap();
+				// assert!(fin.is_none());
 				drop(receiver.take().unwrap());
 				println!("{} receiver /close", i);
 			} else {
@@ -104,12 +127,12 @@ async fn main() {
 				println!("{} receiver /recv", i);
 			}
 			if rng.gen() {
-				delay_for(rng.gen_range(Duration::new(0, 0), Duration::from_millis(1))).await;
+				delay_for(rng.gen_range(Duration::new(0, 0), Duration::from_micros(500))).await;
 			}
 		}
 	};
 	tokio::join!(sending, receiving);
-	tokio::join!(connect_endpoint.wait_idle(), listen_endpoint.wait_idle());
+	tokio::join!(client_endpoint.wait_idle(), server_endpoint.wait_idle());
 
 	println!("done");
 }
